98afefaf658d7bb7772bb0098fc9b82f0653e237
Aghnat Atqiya <aghnat.atqiya@gmail.com>
2021-08-03 10:00:52 +0700

    comment function to update ongkos

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -9,21 +9,21 @@ class UpdateController extends Controller
 {
     public function updateTransportFee(){
 
-        if (auth()->user()->role_id == 1) {
-            # code...
-            $fees = TransportFee::all();
+        // if (auth()->user()->role_id == 1) {
+        //     # code...
+        //     $fees = TransportFee::all();
    
-            // dd("berhasil masuk");
-            foreach ($fees as $item) {
-                $fee = TransportFee::find($item->id);
-                $fee->cost_old_1 = $fee->cost;
-                $fee->cost = $fee->cost + 6000;
-                $fee->save();
-            }
+        //     // dd("berhasil masuk");
+        //     foreach ($fees as $item) {
+        //         $fee = TransportFee::find($item->id);
+        //         $fee->cost_old_1 = $fee->cost;
+        //         $fee->cost = $fee->cost + 6000;
+        //         $fee->save();
+        //     }
     
-            dd("Data updated");
-        } else {
-            dd("Anda tidak punya akses ke halaman ini");
-        }
+        //     dd("Data updated");
+        // } else {
+        //     dd("Anda tidak punya akses ke halaman ini");
+        // }
     }
 }
