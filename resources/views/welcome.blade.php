0d275acbef22ff2ec83edcc53f68885a9c8c70de
Aghnat Atqiya <aghnat.atqiya@gmail.com>
2022-02-04 07:38:44 +0700

    copy login and register button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,6 +42,20 @@
                 </div>
                 <h1>Atqiya's Fun Learning - Education</h1>
 
+                @if (Route::has('login'))
+                    <div class="hidden fixed px-6 py-4 sm:block">
+                        @auth
+                            <a href="{{ url('/Schedule/All') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
